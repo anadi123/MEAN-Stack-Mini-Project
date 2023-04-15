1d92fdec4a78d92cc96270df548fefe26ade560f
shresth-jain-intern-probo <shresth.jain@probo.in>
2023-04-15 16:12:39 +0530

    Add routing for Article/1 detail

--- src/app/staticpages/staticpages-routing.module.ts
+++ src/app/staticpages/staticpages-routing.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AboutUsComponent} from './about-us/about-us.component';
 import {ContactUsComponent} from './contact-us/contact-us.component';
-
+import { ArticleDetailsComponent } from './../articles/article-details/article-details.component'
 const routes: Routes = [
   {path:'about-us', component:AboutUsComponent},
-  {path:'contact-us',component:ContactUsComponent}
+  {path:'contact-us',component:ContactUsComponent},
+  { path: 'article/1', component: ArticleDetailsComponent }
 ];
 
 @NgModule({
